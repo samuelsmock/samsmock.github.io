7da554f41f7a85d8a0c9b6f70e35923b97e2d874
Samuel Smock <smock.samuel@gmail.com>
2020-09-18 21:05:05 -0500

    Updates

---
+++ precache-manifest.b8d2f52403b838a283cd69dfccbb4c5b.js
@@ -0,0 +1,50 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "aae13a518f054b785628b2bd8a96c26f",
+    "url": "/index.html"
+  },
+  {
+    "revision": "b659560ad1d4837cc152",
+    "url": "/static/css/main.73125e23.chunk.css"
+  },
+  {
+    "revision": "fea6a34097d6f8714c59",
+    "url": "/static/js/2.699b3395.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/static/js/2.699b3395.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "b659560ad1d4837cc152",
+    "url": "/static/js/main.8f5a4e9c.chunk.js"
+  },
+  {
+    "revision": "0fbadeffe9e8feb93694",
+    "url": "/static/js/runtime-main.18afb500.js"
+  },
+  {
+    "revision": "73d8e6244d1a6685601d75e0fc632d3f",
+    "url": "/static/media/cypress-darkened.73d8e624.jpg"
+  },
+  {
+    "revision": "de1e9254893c627dd6ec25b52fd8419f",
+    "url": "/static/media/cypress_background.de1e9254.jpg"
+  },
+  {
+    "revision": "1ae0c166b565fd92335483dc6b986c6d",
+    "url": "/static/media/gitHub-white.1ae0c166.png"
+  },
+  {
+    "revision": "2bb7d4dd9f5b8fd805ffbeaaee083fbf",
+    "url": "/static/media/headshot3.2bb7d4dd.jpg"
+  },
+  {
+    "revision": "8c2a56c9b174600a6cea68b96e29562e",
+    "url": "/static/media/linkedIn-white.8c2a56c9.png"
+  },
+  {
+    "revision": "d9ae1cc7688db43a7e0c7be6880610f3",
+    "url": "/static/media/ravenous_thumb.d9ae1cc7.jpg"
+  }
+]);
